1b1ce9bd4eff6fbc266f1a0aca6c4ed55140644a
0da <35634902+0da@users.noreply.github.com>
2018-06-29 07:24:17 +0500

    H2 replaced with SQLite. PngEncoder.

--- src/cc/tantibus/practice2018_5f5dd93/DB.java
+++ src/cc/tantibus/practice2018_5f5dd93/DB.java
@@ -1,85 +1,74 @@
 package cc.tantibus.practice2018_5f5dd93;
 
-import javax.imageio.ImageIO;
+import com.objectplanet.image.PngEncoder;
+
 import java.awt.image.BufferedImage;
 
 import java.io.*;
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 
 /**
  * 0da - 07.06.2018.
  */
-public enum DB {
+public enum DB implements Closeable {
     INSTANCE;
-    private static final String DB_URL = "jdbc:h2:./db";
-    private static final String DB_USER = "root";
-    private static final String DB_PASS = "root";
+    private static final String DB_URL = "jdbc:sqlite:./db.sqlite3";
     private static final String INIT_TABLE = "" +
-            "CREATE TABLE IF NOT EXISTS PUBLIC.IMG\n" +
+            "CREATE TABLE IF NOT EXISTS IMG\n" +
             "(\n" +
-            "    ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,\n" +
-            "    CONTENT BLOB,\n" +
-            "    Y INT NOT NULL,\n" +
-            "    N INT NOT NULL" +
+            "  ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+            "  CONTENT BLOB NOT NULL,\n" +
+            "  VALUE BOOLEAN\n" +
             ");";
-    private static final String PUT_IMAGE = "INSERT INTO PUBLIC.IMG(CONTENT,Y,N) VALUES (?,0,0)";
-    private static final String GET_FEVER_VOTES = "SELECT * FROM PUBLIC.IMG ORDER BY Y + N ASC LIMIT 1";
+    private static final String PUT_IMAGE = "INSERT INTO IMG(CONTENT) VALUES (?)";
 
-
-    private static final String CONTENT = "CONTENT";
-    private static final String YES = "Y";
-    private static final String NO = "N";
-
-    private boolean contentExist = false;
 
     private Connection connection;
-    private PreparedStatement getFewerVotesImageStatement;
-    private ResultSet currentSet;
-
 
     public void init() throws SQLException {
-        org.h2.Driver.load();
-
-        connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
-
-        try (CallableStatement statement = connection.prepareCall(INIT_TABLE)) {
-            statement.execute();
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
 
-        getFewerVotesImageStatement = connection.prepareStatement(GET_FEVER_VOTES, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        connection = DriverManager.getConnection(DB_URL);
 
-        next();
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(INIT_TABLE);
+        }
 
+        connection.setAutoCommit(false);
     }
 
-    Runnable putImage(BufferedImage image) throws IOException, SQLException {
-        return putImage(Collections.singleton(image));
-    }
-
-    Runnable putImage(Collection<BufferedImage> images) throws IOException, SQLException {
+    Runnable putImage(Collection<BufferedImage> images) {
         return () -> {
             try {
-                CallableStatement putImageStatement;
-                synchronized (this) {
-                    putImageStatement = connection.prepareCall(PUT_IMAGE);
-                }
+                PreparedStatement putImageStatement = connection.prepareStatement(PUT_IMAGE);
+                PngEncoder pngEncoder = new PngEncoder();
                 for (BufferedImage image : images) {
                     try {
+                        /*
+                        if (ImageIO.getUseCache()) ImageIO.setUseCache(false);
                         ByteArrayOutputStream os = new ByteArrayOutputStream();
                         ImageIO.write(image, "png", os);
-                        InputStream is = new ByteArrayInputStream(os.toByteArray());
-                        putImageStatement.setBinaryStream(1, is);
+                        putImageStatement.setBytes(1, os.toByteArray());
+                        */
+
+                        ByteArrayOutputStream os = new ByteArrayOutputStream();
+                        pngEncoder.encode(image, os);
+                        putImageStatement.setBytes(1, os.toByteArray());
+
                         putImageStatement.addBatch();
 
-                    } catch (IOException | SQLException e) {
+                    } catch (SQLException | IOException e) {
                         e.printStackTrace();
                     }
                 }
-                synchronized (this) {
-                    putImageStatement.executeBatch();
+                putImageStatement.executeBatch();
+                synchronized (DB.INSTANCE) {
+                    connection.commit();
                 }
                 putImageStatement.close();
             } catch (SQLException e) {
@@ -88,40 +77,15 @@ public enum DB {
         };
     }
 
-    public BufferedImage getImage() throws IOException, SQLException {
-        if (!contentExist) return null;
-        InputStream content = currentSet.getBinaryStream(CONTENT);
-        return ImageIO.read(content);
 
-    }
-
-    public void next(boolean good) throws SQLException {
-        if (contentExist)
-            if (good) {
-                currentSet.updateInt(YES, currentSet.getInt(YES) + 1);
-            } else {
-                currentSet.updateInt(NO, currentSet.getInt(NO) + 1);
-            }
-        next();
-    }
-
-
-    private void next() throws SQLException {
-        if (currentSet != null && contentExist) {
-            currentSet.updateRow();
-            connection.commit();
-        }
-        currentSet = getFewerVotesImageStatement.executeQuery();
-        contentExist = currentSet.first();
-    }
-
-
+    @Override
     public void close() {
-        try {
-            getFewerVotesImageStatement.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        synchronized (DB.INSTANCE) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/cc/tantibus/practice2018_5f5dd93/ImageLoader.java
+++ src/cc/tantibus/practice2018_5f5dd93/ImageLoader.java
@@ -8,10 +8,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.concurrent.*;
-import java.util.function.Function;
 
 
 /**
@@ -23,13 +21,15 @@ public final class ImageLoader extends Thread {
     private static int chunkCount = 0;
     private static int savesCount = 0;
 
-    private static synchronized void addCount(int i) { imageCount++; chunkCount += i; savesCount += i;}
+    private static synchronized void addCount(int i) { imageCount++; chunkCount += i;}
+
+    private static synchronized void addSave(int count) { savesCount+=count; }
 
     private static synchronized void imageDone() { imageCount--; }
 
     private static synchronized void chunkDone() { chunkCount--; }
 
-    private static synchronized void saveDone() { savesCount--; }
+    private static synchronized void saveDone(int count) { savesCount-=count; }
 
     public static int image() { return imageCount; }
 
@@ -37,7 +37,7 @@ public final class ImageLoader extends Thread {
 
     public static int saves() { return savesCount; }
 
-    private ExecutorService service;
+    private final ExecutorService service;
 
     private ImageInputStream stream;
     private ImageReadParam param;
@@ -55,7 +55,7 @@ public final class ImageLoader extends Thread {
     private ImageLoader(String path, int width, int height, int chunkWidth, int chunkHeight) throws IOException {
         super("ImageLoader");
 
-        this.service = Executors.newFixedThreadPool(10);
+        this.service = Executors.newFixedThreadPool(5);
 
         this.stream = ImageIO.createImageInputStream(new File(path));
 
@@ -97,7 +97,7 @@ public final class ImageLoader extends Thread {
 
                     param.setSourceRegion(new Rectangle(x, y, tempW, tempH));
                     BufferedImage result = reader.read(0, param);
-                    save(cutImage(result, width, height));
+                    cutImage(result, width, height);
                     chunkDone();
 
                 } catch (IOException e) {
@@ -118,11 +118,15 @@ public final class ImageLoader extends Thread {
     }
 
 
-    private ArrayList<BufferedImage> cutImage(BufferedImage source, int width, int height) {
+    private void cutImage(BufferedImage source, int width, int height) {
         ArrayList<BufferedImage> result = new ArrayList<>();
+
 
         int sourceWidth = source.getWidth();
         int sourceHeight = source.getHeight();
+
+        width = Math.min(width, sourceWidth);
+        height = Math.min(height, sourceHeight);
 
         for (int i = 0; i < sourceWidth; i += width) {
             if (i + width > sourceWidth) {
@@ -133,19 +137,24 @@ public final class ImageLoader extends Thread {
                     j = sourceHeight - height;
                 }
                 result.add(source.getSubimage(i, j, width, height));
+
+                if (result.size() >= 10) {
+                    save(result);
+                    result = new ArrayList<>();
+                }
             }
         }
 
-        return result;
+        save(result);
     }
 
     private void save(ArrayList<BufferedImage> list) {
-        try {
-            Runnable runnable = DB.INSTANCE.putImage(list);
-            CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(runnable, service);
-            voidCompletableFuture.thenAccept(v -> saveDone());
-        } catch (IOException | SQLException e) {
-            e.printStackTrace();
-        }
+
+        int size = list.size();
+        addSave(size);
+        Runnable runnable = DB.INSTANCE.putImage(list);
+        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(runnable, service);
+        voidCompletableFuture.thenAccept(v -> saveDone(size));
+
     }
 }
